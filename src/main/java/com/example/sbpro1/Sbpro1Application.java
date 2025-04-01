8e419a06e2037befa1223051c52aa8e9805cbe62
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2025-04-01 14:56:21 +0530

    first commit

---
+++ src/main/java/com/example/sbpro1/Sbpro1Application.java
@@ -0,0 +1,13 @@
+package com.example.sbpro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbpro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbpro1Application.class, args);
+	}
+
+}
