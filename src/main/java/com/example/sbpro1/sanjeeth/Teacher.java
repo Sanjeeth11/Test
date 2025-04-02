6d4559dd45063d2191ab4dd4496e2506d2cb9a49
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2025-04-02 12:03:57 +0530

    firstcommit

---
+++ src/main/java/com/example/sbpro1/sanjeeth/Teacher.java
@@ -0,0 +1,42 @@
+package com.example.sbpro1.sanjeeth;
+
+public class Teacher {
+
+	private int teacherId;
+	private String teacherName;
+	private String branch;
+	private int teacherAge;
+
+	public int getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+
+	public String getTeacherName() {
+		return teacherName;
+	}
+
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public int getTeacherAge() {
+		return teacherAge;
+	}
+
+	public void setTeacherAge(int teacherAge) {
+		this.teacherAge = teacherAge;
+	}
+
+}
