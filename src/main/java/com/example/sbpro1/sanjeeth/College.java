497b645bf4e05651b0ad90b09070b5666593b742
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2025-04-04 12:39:20 +0530

    changes

---
+++ src/main/java/com/example/sbpro1/sanjeeth/College.java
@@ -0,0 +1,26 @@
+package com.example.sbpro1.sanjeeth;
+
+public class College {
+
+	private int collegeId;
+	private String collegeName;
+	private long collegeCode;
+	public int getCollegeId() {
+		return collegeId;
+	}
+	public void setCollegeId(int collegeId) {
+		this.collegeId = collegeId;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+	public long getCollegeCode() {
+		return collegeCode;
+	}
+	public void setCollegeCode(long collegeCode) {
+		this.collegeCode = collegeCode;
+	}
+}
---
+++ src/main/java/com/example/sbpro1/sanjeeth/Department.java
@@ -0,0 +1,28 @@
+package com.example.sbpro1.sanjeeth;
+
+public class Department {
+	
+	private int deptId;
+	private String deptName;
+	private String deptLoc;
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getDeptLoc() {
+		return deptLoc;
+	}
+	public void setDeptLoc(String deptLoc) {
+		this.deptLoc = deptLoc;
+	}
+
+	
+}
