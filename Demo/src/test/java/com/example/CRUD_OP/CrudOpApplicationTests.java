5e39a48a49f3b8aedc1c4a32d7aab62a7ba18ca9
gurukrishna1 <gurusaikrishna4@gmail.com>
2025-04-07 14:20:56 +0530

    first commit

---
+++ Demo/src/test/java/com/example/CRUD_OP/CrudOpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_OP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
